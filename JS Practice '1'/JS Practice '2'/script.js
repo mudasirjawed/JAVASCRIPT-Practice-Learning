d14127d18e69e0cf455a6692739f694ada658046
mudas2q <mudasirwindows@gmail.com>
2025-11-05 09:56:45 +0500

    Add JS Practice 02

---
+++ JS Practice '1'/JS Practice '2'/script.js
@@ -0,0 +1,10 @@
+let Name = "Mudasir";
+let age = 22;
+let City = "Sindh, Hyderabad";
+age = 23
+console.log(Name, age, City);
+
+let user_name = prompt("What's Your Name?");
+let user_age = prompt("What's Your Age?");
+console.log("Hi,", user_name, "You are", user_age, "years old.");
+alert("Hi, " + user_name + " You are " + user_age + " years old.");
