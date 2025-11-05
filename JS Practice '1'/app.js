e2f47728e1bf068d9cb9880927e830658acc00ad
mudas2q <mudasirwindows@gmail.com>
2025-11-05 10:24:16 +0500

    JS Practice '1'

---
+++ JS Practice '1'/app.js
@@ -0,0 +1,5 @@
+console.log("Hi, External Applyed!");
+alert("From External JavaScript!");
+console.log("99 - 46 =", 99 - 46);
+console.log("494 + 274 =", 494 + 274);
+console.log("98 X 67 =", 98 * 67);
