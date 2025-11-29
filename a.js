d234afc4acd082a7cbb1651caf08e101da634c25
mudas2q <mudasirwindows@gmail.com>
2025-11-29 09:19:14 +0500

    JS Program 5

---
+++ a.js
@@ -0,0 +1,20 @@
+let a = "Pakistann";
+let b = "urdu";
+
+switch (a) {
+    case "Pakistan":
+        switch (b) {
+            case "urdu":
+                console.log(`yeah you are Pakistani`);
+                break;
+
+                default:
+                    console.log(`you are not speak urdu but ypu are pakistnai`);
+                    
+        }
+        break;
+    default:
+        console.log(`You are Not Pakistani!`);
+
+
+}
